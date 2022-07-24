a4ca61546752cfc23e28787a10f6c50d81ecbf9f
sanjeetsharma1996 <sanjeetsharma1996@gmail.com>
2022-07-24 15:24:31 +0530

    initial commit

---
+++ E_Commerce_API/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+from rest_framework.authtoken.views import obtain_auth_token
+from Books_APP_API.views import RegistrationAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/', include('Books_APP_API.urls')),
+    #path('api/v1/auth/auth-token', obtain_auth_token, name='obtain-auth-token')
+    path('auth/register/', RegistrationAPIView.as_view(), name='register'),
+    path('auth/login/', TokenObtainPairView.as_view(), name='login'),
+    path('auth/refresh-token', TokenRefreshView.as_view(), name='refreshtoken'),
+
+]
---
+++ README.md
@@ -0,0 +1,9 @@
+cd ecommerce-api
+
+pip install django-cors-headers
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py runserver
